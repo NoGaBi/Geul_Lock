01411c35389961ff8be56b5b76364510d096cb98
2017030246 <jho6394@hanyang.ac.kr>
2017-11-24 02:48:48 +0900

    Intro, first page
    main activity, main 2 activity

--- Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
+++ Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
@@ -1,13 +1,22 @@
 package com.geulock.geul_lock;
 
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //overridePendingTransition( R.anim.slideanim, R.anim.endanim );
         setContentView(R.layout.activity_main2);
+
+        Button send_btn = (Button) findViewById(R.id.send_btn);
+        Typeface font = Typeface.createFromAsset(getAssets(), "BRADHITC.TTF");
+        send_btn.setTypeface(font);
+        send_btn.setPaintFlags(send_btn.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
     }
 }
--- Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/MainActivity.java
+++ Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/MainActivity.java
@@ -1,19 +1,49 @@
 package com.geulock.geul_lock;
 
+import android.content.Intent;
+import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition( R.anim.startanim, R.anim.endanim );
         super.onCreate(savedInstanceState);
+
+
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
+        TextView txt1 = (TextView) findViewById(R.id.textView);
+        TextView txt2 = (TextView) findViewById(R.id.textView2);
+        TextView txt3 = (TextView) findViewById(R.id.textView3);
+        Typeface font = Typeface.createFromAsset(getAssets(), "MN.TTF");
+        txt1.setTypeface(font);
+        txt2.setTypeface(font);
+        txt3.setTypeface(font);
+        txt2.setPaintFlags(txt2.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
+        txt3.setPaintFlags(txt3.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
+
+        ConstraintLayout background = (ConstraintLayout) findViewById(R.id.StartBackground);
+
+        background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity( intent );
+                overridePendingTransition( R.anim.slideinanim, R.anim.slideoutanim );
+                finish();
+            }
+        });
+
     }
 }
