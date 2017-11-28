7714a271bf65f9c254e2e613b7289238e2ef4aa5
2017030246 <jho6394@hanyang.ac.kr>
2017-11-28 21:57:12 +0900

    .
    .

--- Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
+++ Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
@@ -4,7 +4,6 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
 
@@ -17,20 +16,12 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition( R.anim.slideinanim, R.anim.endanim );
-        setContentView(R.layout.activity_main2);
-        mRecyclerView = (RecyclerView) findViewById(R.id.msg_recyclerview);
-        Button send_btn = (Button) findViewById(R.id.send_btn);
+        setContentView( R.layout.activity_main2 );
+        Button send_btn = (Button) findViewById( R.id.send_btn );
 
         Typeface font = Typeface.createFromAsset(getAssets(), "BRADHITC.TTF");
         send_btn.setTypeface(font);
         send_btn.setPaintFlags(send_btn.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
 
-        // use a linear layout manager
-        mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(myDataset);
-        mRecyclerView.setAdapter(mAdapter);
     }
 }
