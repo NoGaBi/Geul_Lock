c833bd713bbf444c275545367bcb0cbdd3f9337e
2017030246 <jho6394@hanyang.ac.kr>
2017-11-30 19:35:47 +0900

    font edited

--- Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
+++ Application/Geul_Lock/app/src/main/java/com/geulock/geul_lock/Main2Activity.java
@@ -4,23 +4,24 @@ import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Main2Activity extends AppCompatActivity {
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         overridePendingTransition( R.anim.slideinanim, R.anim.endanim );
         setContentView( R.layout.activity_main2 );
+
         Button send_btn = (Button) findViewById( R.id.send_btn );
+        EditText Main2_intput_text = (EditText) findViewById( R.id.main2_editText);
 
         Typeface font = Typeface.createFromAsset(getAssets(), "BRADHITC.TTF");
+        Typeface font2 = Typeface.createFromAsset(getAssets(), "MN.TTF");
         send_btn.setTypeface(font);
+        Main2_intput_text.setTypeface(font2);
         send_btn.setPaintFlags(send_btn.getPaintFlags() | Paint.FAKE_BOLD_TEXT_FLAG);
 
     }
