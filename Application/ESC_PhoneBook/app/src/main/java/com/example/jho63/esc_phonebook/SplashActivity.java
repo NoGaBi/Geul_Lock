5f44e841b3de327700768e90d7111c00914fb21c
2017030246 <jho6394@hanyang.ac.kr>
2018-07-08 15:20:36 +0900

    새 프로젝트 생성 | 기존 프로젝트 스플래쉬 애니메이션 추가
    스플래쉬에 애니메이션을 추가하였으나, 야매?적인 방법이라 정상적인 작동은 안됨.
    Handler나 http://dudmy.net/android/2017/04/09/improved-loading-screen/ 에서 쓰는 방법을 이용한 스플래쉬 이벤트가 필요해 보임. 이에 새 프로젝트 생성, 스플래쉬의 경우 완성 후 추가하는게 나을 거 같다는 판단.

--- Application/ESC_PhoneBook/app/src/main/java/com/example/jho63/esc_phonebook/SplashActivity.java
+++ Application/ESC_PhoneBook/app/src/main/java/com/example/jho63/esc_phonebook/SplashActivity.java
@@ -1,7 +1,13 @@
 package com.example.jho63.esc_phonebook;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        overridePendingTransition(R.anim.spalsh_start,0);
+
+        ConstraintLayout background = (ConstraintLayout) findViewById(R.id.SplashBackground);
+        final Animation splashEnd = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.splash_finish);
+
+        background.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                ImageView splashImg = (ImageView) findViewById(R.id.SplashImage);
+                splashImg.startAnimation(splashEnd);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
